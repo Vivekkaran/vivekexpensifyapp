f719a03a6ec4eb5cb4fd39adde8bfeb531a8fa09
Vivek Karunakaran <vivek.karunakaran@ibm.com>
2018-06-26 15:33:58 -0300

    Setup test database environment

---
+++ src/firebase/firebase.js
@@ -0,0 +1,105 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // database.ref('expenses')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const expenses = [];
+// //     snapshot.forEach((data) => {
+// //       expenses.push({
+// //         id: data.key,
+// //         ...data.val()
+// //       });
+// //     });
+// //     console.log(expenses);
+// //   });
+
+// // database.ref('expenses').on('value', (snapshot) => {
+// //   const expenses = [];
+// //   snapshot.forEach((data) => {
+// //     expenses.push({
+// //       id: data.key,
+// //       ...data.val()
+// //     });
+// //   });
+// //   console.log(expenses);
+// // });
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 976123498763
+// });
+
+
+
+
+
+// // database.ref().on('value', (snapshot) => {
+// //   console.log(`${snapshot.val().name} is a ${snapshot.val().job.title} at ${snapshot.val().job.company}`);
+// // }, (e) => {
+// //   console.log('Error with data fetching', e);
+// // });
+
+
+
+// // database.ref().set({
+// //   name: 'Vivek Karunakaran',
+// //   age: 26,
+// //   stressLevel: 5,
+// //   job: {
+// //     title: 'Software Developer',
+// //     company: 'Facebook'
+// //   },
+// //   location: {
+// //     city: 'Halifax',
+// //     country: 'Canada'
+// //   }
+// // }).then(() => {
+// //   console.log('Data is saved!');
+// // }).catch((e) => {
+// //   console.log('This failed', e);
+// // });
+
+// // database.ref()
+// // .update({
+// //   stressLevel: 9,
+// //   'job/company': 'Amazon',
+// //   'location/city': 'Vancouver'
+// // });
+
+// // database.ref()
+// // .remove()
+// // .then(() => {
+// //   console.log('Removed the data');
+// // })
+// // .catch((e) => {
+// //   console.log('Remove Error', e);
+// // });
