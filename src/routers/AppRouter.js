a283e36a5a7946a4155e4b19b55b65878a8fcc0c
Vivek Karunakaran <vivek.karunakaran@ibm.com>
2018-06-27 13:49:47 -0300

    Add private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
-import Header from '../components/Header'
-import AddExpensePage from '../components/AddDashboard'
-import EditExpensePage from '../components/EditDashboard'
-import ExpenseDashboardPage from '../components/ExpenseDashboard'
-import HelpPage from '../components/HelpDashboard'
-import NotFoundPage from '../components/NotFound'
+import React from 'react';
+import { Router, Route, Switch, Link, NavLink} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import AddExpensePage from '../components/AddDashboard';
+import EditExpensePage from '../components/EditDashboard';
+import ExpenseDashboardPage from '../components/ExpenseDashboard';
+import HelpPage from '../components/HelpDashboard';
+import NotFoundPage from '../components/NotFound';
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createHistory();
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
-      <Header /> <br/>
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
+        <Route path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
         {/* <Route path="/help" component={HelpPage} /> */}
         <Route component={NotFoundPage} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 )
 
 export default AppRouter;
